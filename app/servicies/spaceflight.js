0525e7eeefedd5ff8193f26d48f9120230897ea2
Lucas Aldazabal <lucas@aldazabal.com.ar>
2024-04-29 00:09:46 -0300

    change servicies and dashboards

--- app/servicies/spaceflight.js
+++ app/servicies/spaceflight.js
@@ -4,7 +4,10 @@ import { measureExecution } from "../utils/metrics.js";
 export async function getSpaceflightNews() {
   try {
     const data = await measureExecution("external_api_time", async () => {
-      const aux = await fetch(`https://api.spaceflightnewsapi.net/v4/articles/?limit=5`);
+      const aux = await fetch(`https://api.spaceflightnewsapi.net/v4/articles/?format=json&limit=5`);
+      if (!aux.ok) {
+        throw new HttpError("Could not get latests news", aux.status);
+      }
       const data = await aux.json();
       return data;
     });
@@ -12,7 +15,7 @@ export async function getSpaceflightNews() {
 
     return titles;
   } catch (error) {
-    console.error(error);
+    console.error(error.code, error.message);
     throw new HttpError("Could not get latests news", 500);
   }
 }
